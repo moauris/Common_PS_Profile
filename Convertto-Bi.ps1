da43673fd269f9df405e0226986f2a1eeaab06db
moauris <mchenf@icloud.com>
2019-01-24 13:27:34 +0900

    first commit

---
+++ Convertto-Bi.ps1
@@ -0,0 +1,14 @@
+﻿Param(
+    [int]$int1
+)
+$moduler
+$reminder = $int1
+$res
+
+do {
+    $moduler = $reminder % 2
+    $reminder = ($reminder - $moduler) / 2
+    $res = [string] $res + $moduler
+}
+until($reminder = 0)
+$res
